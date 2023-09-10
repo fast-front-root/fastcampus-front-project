e71c9e772a727b9cf33110fe71c57c6a649c3eae
fast-front-root <en.eastroots92@gmail.com>
2023-09-10 14:30:14 +0900

    프로젝트 생성 & 환경 설정 (2) - layout 패키지 세팅

---
+++ packages/react/components/layout/src/Block.tsx
@@ -0,0 +1,5 @@
+import { blockStyle } from "./block.css";
+
+export const Block = () => {
+  return <div className={blockStyle}>안녕하세요</div>;
+};
---
+++ packages/react/components/layout/src/block.css.ts
@@ -0,0 +1,8 @@
+import { vars } from "@fastcampus/themes";
+import { style } from "@vanilla-extract/css";
+
+export const blockStyle = style({
+  width: '100px',
+  height: '100px',
+  backgroundColor: vars.colors.$scale.blue[500],
+});
