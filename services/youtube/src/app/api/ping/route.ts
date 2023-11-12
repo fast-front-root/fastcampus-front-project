d760782efb0a609c8843ffaa67b7f1de2477c75b
fast-front-root <en.eastroots92@gmail.com>
2023-11-12 17:31:06 +0900

    유튜브 Data API V3 소개

--- services/youtube/src/app/api/ping/route.ts
+++ services/youtube/src/app/api/ping/route.ts
@@ -1,10 +1,15 @@
 import { NextRequest } from "next/server";
 
-export const GET = async (request: NextRequest) => {
-  console.log('req', request);
+export const GET = async () => {
+  const API_KEY = process.env.YOUTUBE_API_KEY;
+  const res = await fetch(
+    `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=KR&key=${API_KEY}`,
+  );
+
+  const data = await res.json();
 
   return Response.json({
-    value: 'pong~!@#',
+    data,
   });
 }
 
